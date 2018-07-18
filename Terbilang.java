fd5080718a008e0874734e6f795b50d6c2229cb5
Taufik Arifuddin <taufikarifuddin@gmail.com>
2018-07-18 08:14:36 +0700

    init

---
+++ Terbilang.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package terbilang;
+
+/**
+ *
+ * @author Taufik A
+ */
+public class Terbilang {
+
+    public static void main(String[] args) {
+        TerbilangConverter terbilang = new TerbilangConverter();
+        System.out.println(terbilang.doConvert(233200));
+    }
+    
+}
